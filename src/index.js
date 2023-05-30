70aa70f267fe664d72e907e16b8ab73f05e2211f
unknown <chris.olivosf@gmail.com>
2023-05-30 18:24:42 -0400

    rutas agregadas

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import DashboardView from './components/dashboardView'
+import NewBook from './components/newBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+  <Routes>
+    {/* cada componente route indica la ruta */}
+    <Route path="/" element={<App />} />
+    <Route path="dashboard" element={<DashboardView />} />
+    <Route path="addbook" element={<NewBook />} />
+      {/* <Route path="dashboard/:uid" element={<DashboardView />} /> */}
+      {/* <Route path="dashboard/profile" element={<EditProfile />} */}
+
+  </Routes>
+</BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
